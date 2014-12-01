791a3f9ab3260c52c8b18e603734cdc72ccb61b5
alber sánchez <albhasan@gmail.com>
2014-12-01 17:15:01 +0100

    Update containerSetup.sh
    Run additional scripts

--- containerSetup.sh
+++ containerSetup.sh
@@ -14,5 +14,7 @@ export LC_ALL="en_US.UTF-8"
 sudo -u postgres createdb postgisDB
 sudo -u postgres psql -d postgisDB -c "CREATE EXTENSION postgis;" 
 sudo -u postgres psql -d postgisDB -c "CREATE EXTENSION postgis_topology;" 
-sudo -u postgres psql -d postgisDB -f /usr/share/postgresql/9.3/contrib/postgis-2.1/legacy.sql
+#sudo -u postgres psql -d postgisDB -f /usr/share/postgresql/9.3/contrib/postgis-2.1/legacy.sql
+sudo -u postgres psql -d postgisDB -f /usr/share/postgresql/9.3/contrib/postgis-2.1/postgis.sql
+sudo -u postgres psql -d postgisDB -f /usr/share/postgresql/9.3/contrib/postgis-2.1/spatial_ref_sys.sql
 
